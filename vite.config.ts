e6cbaddd93da66dceb27af03f6c973229bc9a5c6
dwidge <dwidge+gh@gmail.com>
2023-11-13 18:53:25 +0200

    Fix vite env

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 import process from "node:process";
-const { env } = process;
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd());
   return {
     base: env.VITE_BASE_URL,
     plugins: [
